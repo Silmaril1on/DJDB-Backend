303df023c90bfdba72013ca1ae1cdb284e67decc
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2025-03-02 18:59:12 +0400

    c

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://electorinc.vercel.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://djdb.vercel.app",
+      "https://electorinc.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
