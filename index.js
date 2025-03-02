2e34f5053e110b8299e4dcf596ab0a66ea383e46
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2025-03-02 18:27:08 +0400

    c

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://electorinc.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
