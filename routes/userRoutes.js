3443b603ddb1a8bc7eee2d943ac2c5b81f1048ad
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2025-03-04 22:50:59 +0400

    c

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,3 @@
-// const express = require("express");
-// const requireAuth = require("../middleware/requireAuth");
-// const router = express.Router();
-// const {
-//   onLoginUser,
-//   onSignUpUser,
-//   getUserDetails,
-//   updateUser,
-// } = require("../controllers/userControllers");
-
-// router.post("/login", onLoginUser);
-// router.post("/signup", onSignUpUser);
-// router.get("/details", requireAuth, getUserDetails);
-// router.put("/update", requireAuth, updateUser);
-
-// module.exports = router;
-
 const express = require("express");
 const requireAuth = require("../middleware/requireAuth");
 const router = express.Router();
